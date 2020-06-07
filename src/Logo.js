850cf3f34505d0cb26d4de4512e897b122a69123
Oliver Carlsson <oliverc@Olivers-Air.lan>
2020-06-07 17:22:22 +0200

    Remove standard template

---
+++ src/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function Logo() {
+    return (
+        <div>
+            <img src={logo} alt="OCD"/>
+        </div>
+    );
+}
+
+export default Logo;
