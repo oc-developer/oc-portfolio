7cce6c38f28e86a46aa2131121356201a939d4bc
Oliver Carlsson <oliverc@Olivers-Air.lan>
2020-06-07 17:51:14 +0200

    Add floaty letter component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Logo from './Logo';
+import FloatyLetter from './FloatyLetters/FloatyLetters'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <Logo/>
+        <FloatyLetter/>
     </div>
   );
 }
---
+++ src/FloatyLetters/FloatyLetters.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function FloatyLetters(props) {
+    return (
+        <div>Hello</div>
+    );
+}
+
+export default FloatyLetters;
