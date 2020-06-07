93625105b8f538cfcfa7c2a00b61e7afd2969f57
Oliver Carlsson <oliverc@Olivers-Air.lan>
2020-06-07 18:57:23 +0200

    Measure dom elements

--- src/FloatyLetters/FloatyLetters.js
+++ src/FloatyLetters/FloatyLetters.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from 'react'
 import './FloatyLetters.css'
+import useDimensions from 'react-use-dimensions'
+
 
 function FloatyLetters(props) {
+    const [ref, letterSize] = useDimensions();
+    console.log("width", letterSize.width);
+
+    const letterStyle = {
+        width: letterSize.width,
+        height: letterSize.width
+    }
+
     return (
-        <div className='floaty-box'>
+        <div className='floaty-box' style={letterStyle}>
             <div className='floaty-ani-x' style={{ animationDuration: '2s' }}>
                 <div className='floaty-ani-y' style={{ animationDuration: '2s' }}>
-                    <div className='floaty-letter'>H</div>
+                    <div ref={ref} className='floaty-letter'>H</div>
                 </div>
             </div>
         </div>
