acafc7bf3d42c8150742ba77fd66da6c7c77e560
Oliver Carlsson <oliverc@Olivers-Air.lan>
2020-06-07 18:11:42 +0200

    Intial moving letter

--- src/FloatyLetters/FloatyLetters.js
+++ src/FloatyLetters/FloatyLetters.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import './FloatyLetters.css'
 
 function FloatyLetters(props) {
     return (
-        <div>Hello</div>
+        <div className="floaty-ani-x" style={{ animationDuration: '2s' }}>
+            <div className="floaty-ani-y" style={{ animationDuration: '2s' }}>
+                <div className="floaty-letter">H</div>
+            </div>
+        </div>
     );
 }
 
