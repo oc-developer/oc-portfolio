6a11c58fc35be1713a273d61e8c0c73a7073df2b
Oliver Carlsson <oliverc@Olivers-Air.lan>
2020-07-08 08:35:27 +0200

    Add about

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -4,8 +4,20 @@ import './Home.css'
 
 function Home() {
     return (
-        <div className="welcome-banner">
-            <FloatyLetters word="Welcome!" maxDuration={15} minDuration={5} />
+        <div className="home">
+            <div className="welcome-banner">
+                <FloatyLetters word="Welcome!" maxDuration={15} minDuration={5} />
+            </div>
+            <div className="about">
+                Hello internet!<br />
+                I’m a social software engineer with special love for web-based application development.
+                I enjoy being at the heart of the action, my passion is to bring people together and have fun.
+                I’ve studied software engineering and Interaction design and I’m fascinated by the synergy that occurs between good tooling and people.
+                My passion is in user experience and front-end development and hope to continue to explore the amazing world of web-based technology and develop as a good team-player.<br />
+                <br />
+                In my spare time I enjoy the outdoors, either the wonderful climbing walls around Gothenburg and Bohuslän or being out on the water when the wind kicks in kitesurfing.
+                I also enjoy music and singing together with friends.
+        </div>
         </div>
     )
 }
