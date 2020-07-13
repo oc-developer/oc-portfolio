7e6bce2cdc18acb03d7c1c7fb1ca682eff80db1f
Oliver Carlsson <oliverc@Olivers-MacBook-Air.local>
2020-07-13 17:21:54 +0200

    Create floaty letter class

---
+++ src/FloatyLetters/FloatyLetter/FloatyLetter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '.FloatyLetter.css'
+
+export default class FloatyLetter extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
