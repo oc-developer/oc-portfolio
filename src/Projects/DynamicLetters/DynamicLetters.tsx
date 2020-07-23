25de4e231b7b22e33d59e408a7ed6fb7e1795198
Oliver Carlsson <oliverc@Olivers-Air.lan>
2020-07-23 11:45:02 +0200

    Slider fn

--- src/Projects/DynamicLetters/DynamicLetters.tsx
+++ src/Projects/DynamicLetters/DynamicLetters.tsx
@@ -6,6 +6,7 @@ export default function DynamicLetters() {
     const placeholder = 'Example...'
     const [phrase, setPhrase] = useState('')
     const [Speed, setSpeed] = useState(1)
+    const [SliderScale, setSliderScale] = useState(0)
 
     const setPhraseDefault = () => {
         return phrase || placeholder;
@@ -17,11 +18,16 @@ export default function DynamicLetters() {
             <span>I wanted to create an organic animation to websites by creating motion to letters.</span>
             <br />
             <input type="text" placeholder={placeholder} value={phrase} onChange={e => setPhrase(e.target.value)} />
-            <br/>
-            <label>{Speed}</label>
-            <input type="range" value={Speed} step={0.1} min={0.1} max={10} onChange={e => setSpeed(Number(e.target.value))} />
-            <br/>
-            <FloatyLetters phrase={setPhraseDefault()} maxDuration={15*Speed} minDuration={5*Speed} />
+            <br />
+            <label>Speed factor: {Speed.toFixed(2)}</label>
+            <br />
+            <input type="range" value={SliderScale} step={1} min={-4} max={4} onChange={e => {
+                const value = Number(e.target.value)
+                setSliderScale(value)
+                setSpeed(Math.exp(value))
+            }} />
+            <br />
+            <FloatyLetters phrase={setPhraseDefault()} maxDuration={15 / Speed} minDuration={5 / Speed} />
         </div>
     )
 }
