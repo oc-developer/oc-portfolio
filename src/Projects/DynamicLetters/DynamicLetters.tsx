dce22e56a87802297809ed3dad96e5fdb4075d12
Oliver Carlsson <oliverc@Olivers-Air.lan>
2020-07-24 15:33:11 +0200

    Add explenation to floaty-letter package

--- src/Projects/DynamicLetters/DynamicLetters.tsx
+++ src/Projects/DynamicLetters/DynamicLetters.tsx
@@ -15,7 +15,92 @@ export default function DynamicLetters() {
     return (
         <div className="dynamic-letters">
             <h1>Dynamic Letters</h1>
-            <span>I wanted to create an organic animation to websites by creating motion to letters.</span>
+            <span>
+                I wanted to make letters part of the dynamic experience in websites.
+                To make the site more alive I created an organic library that randomize
+                motion according to a few restrictions. <br /><br />
+            </span>
+            <span>
+                Floaty letter is based on css animation and uses an infinite loop of translate. It contains three boxes:
+                The X-Box moving the letter in the x-axis.
+                </span>
+            <div className="floaty-demo">
+                <div className="demo-box x-box"></div>
+            </div>
+            <span>
+                The Y-box moving the letter in y-axis.
+            </span>
+            <div className="floaty-demo">
+                <div className="demo-box y-box"></div>
+            </div>
+            <span>
+                ...and the letter.
+            </span>
+            <div className="floaty-demo">
+                <div className="demo-box letter-box">
+                    A
+                </div>
+            </div>
+            <span>
+                Putting these boxes together create an moving letter, but not very organic.
+            </span>
+            <div className="floaty-demo">
+                <div className="demo-box x-box">
+                    <div className="demo-box y-box">
+                        <div className="demo-box letter-box">A</div>
+                    </div>
+                </div>
+            </div>
+            <span>
+                ...however having different animation durations on the x and y-axis creates the feeling of the letter moving in an arbitrary way.
+            </span>
+            <div className="floaty-demo">
+                <div className="demo-box x-box slow-box">
+                    <div className="demo-box y-box">
+                        <div className="demo-box letter-box">A</div>
+                    </div>
+                </div>
+            </div>
+            <span>
+                With no background colors it looks even better.
+            </span>
+            <div className="floaty-demo">
+                <div className="demo-box x-box slow-box no-color">
+                    <div className="demo-box y-box no-color">
+                        <div className="demo-box letter-box no-color">A</div>
+                    </div>
+                </div>
+            </div>
+            <span>
+                Using this technique when creating a phrase shows that it is important to have variation for each letter.
+            </span>
+            <div className="floaty-demo">
+                <div className="demo-box x-box slow-box no-color">
+                    <div className="demo-box y-box no-color">
+                        <div className="demo-box letter-box no-color">H</div>
+                    </div>
+                </div>
+                <div className="demo-box x-box slow-box no-color">
+                    <div className="demo-box y-box no-color">
+                        <div className="demo-box letter-box no-color">e</div>
+                    </div>
+                </div>
+                <div className="demo-box x-box slow-box no-color">
+                    <div className="demo-box y-box no-color">
+                        <div className="demo-box letter-box no-color">l</div>
+                    </div>
+                </div>
+                <div className="demo-box x-box slow-box no-color">
+                    <div className="demo-box y-box no-color">
+                        <div className="demo-box letter-box no-color">l</div>
+                    </div>
+                </div>
+                <div className="demo-box x-box slow-box no-color">
+                    <div className="demo-box y-box no-color">
+                        <div className="demo-box letter-box no-color">o</div>
+                    </div>
+                </div>
+            </div>
             <div className="settings">
                 <input className="dynamic-text-input" type="text" placeholder={placeholder} value={phrase} onChange={e => setPhrase(e.target.value)} />
                 <br />
