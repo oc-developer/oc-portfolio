539e17c6bd5b4d212f5600fd3a473a9f8df0ae6a
Oliver Carlsson <olivercarlsson@iMac.lan>
2020-07-27 00:17:13 +0200

    add robot and animation

---
+++ src/Protagonist/Protagonist.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Protagonist.css'
+
+import Logo from '../NewLogo/Logo'
+
+export default function Protagonist() {
+    return (
+        <div className="section protagonist-section">
+            <Logo/>
+        </div>
+    )
+} 
