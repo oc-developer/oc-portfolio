b788340aa4dfb929057a93f9077b37794b898b04
Oliver <olivercarlsson89@gmail.com>
2020-07-27 13:54:56 +0200

    Protagonist
    * add robot and animation
    
    * First draft robot ani
    
    * fine tune animation
    
    * add mini logo
    
    * minify new logo
    
    * Add developer notes

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import './App.css'
 import Header from './Header/Header'
 import About from './About/About'
 import DynamicLetters from './Projects/DynamicLetters/DynamicLetters'
+import Protagonist from './Protagonist/Protagonist'
 
 function App() {
   return (
     <div className="app">
+      <Protagonist />
       <div className="main">
         <Header />
         <div className="content">
---
+++ src/Protagonist/Protagonist.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Protagonist.css'
+
+import Logo from '../NewLogo/Logo'
+
+export default function Protagonist() {
+    return (
+        <div className="section protagonist-section">
+            <Logo/>
+            <h1 className="fade-in">Oliver Carlsson Developer Notes</h1>
+        </div>
+    )
+} 
