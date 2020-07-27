2c1901ede767da1715b18c8a9150c391bbb1fd69
Oliver Carlsson <olivercarlsson@iMac.lan>
2020-07-27 14:00:12 +0200

    Create notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import './App.css'
-import Header from './Header/Header'
 import About from './About/About'
 import DynamicLetters from './Projects/DynamicLetters/DynamicLetters'
 import Protagonist from './Protagonist/Protagonist'
+import Notes from './Notes/Notes'
 
 function App() {
   return (
     <div className="app">
       <Protagonist />
+      <Notes />
       <div className="main">
-        <Header />
         <div className="content">
           <About />
           <DynamicLetters />
---
+++ src/Notes/Notes.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Notes.css'
+
+export default function Notes() {
+    return (
+        <div>Notes</div>
+    )
+}
