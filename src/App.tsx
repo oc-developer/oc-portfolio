facf898d51af63750f072e0e6371e1b73335417d
Oliver <olivercarlsson89@gmail.com>
2020-07-30 14:59:14 +0200

    OC dev logo animation (#5)
    * Animations to mini logo
    
    * new mini logo behavior
    
    * remove old animation
    
    * New logo animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import './App.scss'
+import './Animations.scss'
 
 import Header from './Header/Header'
 import Main from './Main/Main'
