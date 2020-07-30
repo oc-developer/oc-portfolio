446d673900a9e43cfe97b3b18146652747699a8f
Oliver Carlsson <oliverc@Olivers-MacBook-Air.local>
2020-07-30 17:00:00 +0200

    Add pic and breif

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -1,22 +1,23 @@
 import React from 'react'
-import FloatyLetters from '../FloatyLetters/FloatyLetters'
 import './About.scss'
+import me from './oliver-mini.jpeg'
 
 export default function About() {
     return (
-        <div className="section about-section">
-            <div className="welcome-banner">
-                <FloatyLetters phrase="HI THERE..." maxDuration={15} minDuration={5} />
+        <div className="section grid-items section-about">
+            <h2 className="section-header">me</h2>
+            <div className="section-content about-me">
+                <div className="left">
+                    <h3 className="header">Oliver Carlsson</h3>
+                    <div className="dash"></div>
+                    <span className="breif">I’m a social developer, I love to create and to bring teams closer together. 
+                        I design, develop, create and share my passion. 
+                        You got an interesting project? Please say hello!</span>
+                </div>
+                <div className="right">
+                    <img className="image-me" src={me} alt="Oliver" />
+                </div>
             </div>
-            <span className="about">
-                I’m a social software engineer with special love for web-based application development.
-                I enjoy being at the heart of the action, my passion is to bring people together and have fun.
-                I’ve studied software engineering and Interaction design and I’m fascinated by the synergy that occurs between good tooling and people.
-                My passion is in user experience and front-end development and hope to continue to explore the amazing world of web-based technology and develop as a good team-player.<br />
-                <br />
-                In my spare time I enjoy the outdoors, either the wonderful climbing walls around Gothenburg and Bohuslän or being out on the water when the wind kicks in kitesurfing.
-                I also enjoy music and singing together with friends.
-            </span>
         </div>
     )
 }
