b2c8f260c15499cd406402383761365a25e35f83
Oliver Carlsson <oliverc@Olivers-MacBook-Air.local>
2020-08-04 21:25:25 +0200

    wcar

--- src/Work/Work.tsx
+++ src/Work/Work.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Work.scss'
 
 import ictechLogo from '../Utils/ictech.png'
+import wcar from '../Utils/wirelesscar-white.svg'
 
 export default function Work() {
     return (
@@ -9,14 +10,15 @@ export default function Work() {
             <h2 className="section-header">Work</h2>
             <div className="section-content section-work">
                 <div className="half">
-                    
-                        <a href="https://ictech.se/" className="work-place ictech">
-                            <img className="ictech-logo" src={ictechLogo} alt="ictech" />
-                            <span> I'm currently a consultant at ictech, we are a creative tech-company for the software development industry, based out of Gothenburg.</span>
-                        </a>
-                    <div className="work-place wireless-car">
-                        <h2>WirelessCar</h2>
-                    </div>
+
+                    <a href="https://ictech.se/" className="work-place ictech">
+                        <img className="ictech-logo" src={ictechLogo} alt="ictech" />
+                        <span> I'm currently a consultant at ictech, we are a creative tech-company for the software development industry, based out of Gothenburg.</span>
+                    </a>
+                    <a href="https://www.wirelesscar.com/" className="work-place wireless-car">
+                        <img className="wcar-logo" src={wcar} alt="Wireless Car" />
+                        <span>Lead front-end developer for wireless car call center services developing and modernize the platform and its user experience.</span>
+                    </a>
                 </div>
                 <div className="work-place">
                     <h2>Tibco Spotfire</h2>
