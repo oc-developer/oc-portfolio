5434938f3243b8919e25e787d40a70aa8dd84641
Oliver Carlsson <oliverc@Olivers-MacBook-Air.local>
2020-08-04 22:41:19 +0200

    add hover styling

--- src/Work/Work.tsx
+++ src/Work/Work.tsx
@@ -12,16 +12,16 @@ export default function Work() {
             <div className="section-content section-work">
                 <div className="half">
 
-                    <a href="https://ictech.se/" className="work-place section-hover ictech">
+                    <a href="https://ictech.se/" className="work-place work-hover ictech">
                         <img className="ictech-logo" src={ictechLogo} alt="ictech" />
                         <span> I'm currently a consultant at ictech, we are a creative tech-company for the software development industry, based out of Gothenburg.</span>
                     </a>
-                    <a href="https://www.wirelesscar.com/" className="work-place section-hover wireless-car">
+                    <a href="https://www.wirelesscar.com/" className="work-place work-hover wireless-car">
                         <img className="wcar-logo" src={wcar} alt="Wireless Car" />
                         <span>Lead front-end developer for wireless car call center services developing and modernize the platform and its user experience.</span>
                     </a>
                 </div>
-                <a href="https://www.tibco.com/products/tibco-spotfire" className="work-place section-hover tibco">
+                <a href="https://www.tibco.com/products/tibco-spotfire" className="work-place work-hover tibco">
                     <img src={tibco} alt="TIBCO" />
                     <span>Tibco Spotfire Application developer for the Data Analytics platform.</span>
                 </a>
