b4f12e89f0cabe3c1478b5fd76dcd2920004defa
Oliver Carlsson <oliverc@Olivers-MacBook-Air.local>
2020-08-05 21:13:02 +0200

    Grid layout for work

--- src/Work/Work.tsx
+++ src/Work/Work.tsx
@@ -10,19 +10,16 @@ export default function Work() {
         <div className="section grid-items work">
             <h1 className="section-header">work</h1>
             <div className="section-content section-work">
-                <div className="half">
-
-                    <a href="https://ictech.se/" className="work-place work-hover ictech">
-                        <img className="ictech-logo" src={ictechLogo} alt="ictech" />
-                        <p> I'm currently a consultant at ictech, we are a creative tech-company for the software development industry, based out of Gothenburg.</p>
-                    </a>
-                    <a href="https://www.wirelesscar.com/" className="work-place work-hover wireless-car">
-                        <img className="wcar-logo" src={wcar} alt="Wireless Car" />
-                        <p>Lead front-end developer for wireless car call center services developing and modernize the platform and its user experience.</p>
-                    </a>
-                </div>
+                <a href="https://ictech.se/" className="work-place work-hover ictech">
+                    <img className="logo" src={ictechLogo} alt="ictech" />
+                    <p> I'm currently a consultant at ictech, we are a creative tech-company for the software development industry, based out of Gothenburg.</p>
+                </a>
+                <a href="https://www.wirelesscar.com/" className="work-place work-hover wireless-car">
+                    <img className="logo" src={wcar} alt="Wireless Car" />
+                    <p>Lead front-end developer for wireless car call center services developing and modernize the platform and its user experience.</p>
+                </a>
                 <a href="https://www.tibco.com/products/tibco-spotfire" className="work-place work-hover tibco">
-                    <img src={tibco} alt="TIBCO" />
+                    <img className="logo" src={tibco} alt="TIBCO" />
                     <p>Tibco Spotfire Application developer for the Data Analytics platform.</p>
                 </a>
             </div>
