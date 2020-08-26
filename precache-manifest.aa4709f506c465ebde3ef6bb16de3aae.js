90e03a3aa1cab1f9a203970e7e16488e4429ceae
Oliver Carlsson <oliverc@Olivers-Air.lan>
2020-08-26 20:04:17 +0200

    Updates

---
+++ precache-manifest.aa4709f506c465ebde3ef6bb16de3aae.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f92b790d5fd7ad8b0bdad6863c6992f1",
+    "url": "/oc/index.html"
+  },
+  {
+    "revision": "fe1a0b4df0185e1a3886",
+    "url": "/oc/static/css/main.f3d3b0e3.chunk.css"
+  },
+  {
+    "revision": "07211169697f7dcef1cc",
+    "url": "/oc/static/js/2.c646de5e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/oc/static/js/2.c646de5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe1a0b4df0185e1a3886",
+    "url": "/oc/static/js/main.ab65d6d7.chunk.js"
+  },
+  {
+    "revision": "45d373157aa9aaa12f35",
+    "url": "/oc/static/js/runtime-main.bf858d27.js"
+  },
+  {
+    "revision": "569d6308758bd5bbc82e79e2004bf442",
+    "url": "/oc/static/media/Twitter-logo.569d6308.svg"
+  },
+  {
+    "revision": "5871602b3cce68db0632be9353b43e15",
+    "url": "/oc/static/media/github-mark.5871602b.svg"
+  },
+  {
+    "revision": "e0d4c1478280d8038e97e2fa270f7ad9",
+    "url": "/oc/static/media/ictech.e0d4c147.png"
+  },
+  {
+    "revision": "dd98389508c63550a9bd6f3043b261d6",
+    "url": "/oc/static/media/linkedin.dd983895.svg"
+  },
+  {
+    "revision": "0582156d8bda3d20659788270a36c224",
+    "url": "/oc/static/media/oliver-mini.0582156d.jpeg"
+  },
+  {
+    "revision": "9df85f692fa7960907a0a5d7b95aa6b4",
+    "url": "/oc/static/media/tibco.9df85f69.svg"
+  },
+  {
+    "revision": "5f06ade075e79a3d6fadf77745f80108",
+    "url": "/oc/static/media/wirelesscar-white.5f06ade0.svg"
+  }
+]);
